9740428a4e6947c8029cd7fdfca66efc3be02865
Niclas Haderer <niclas.haderer.dev@gmail.com>
2022-01-05 12:39:05 +0100

    Fixes #17

--- src/main/kotlin/audible/client/AudibleClient.kt
+++ src/main/kotlin/audible/client/AudibleClient.kt
@@ -7,11 +7,13 @@ import audible.models.AudibleSearchLanguage
 import audible.models.AudibleSearchResult
 import audible.models.AudibleSeries
 import io.ktor.client.*
+import me.xdrop.fuzzywuzzy.FuzzySearch
 
 open class AudibleClient(
     private val searchHost: String = "audible.de",
     private val authorHost: String = "audible.de",
     private val authorImageSize: Int = 500,
+    private val searchScore: Int = 80,
 ) {
     private val client = HttpClient()
 
@@ -41,8 +43,25 @@ open class AudibleClient(
     suspend fun getAuthorByName(authorName: String): AudibleAuthor? {
         val handler = SearchHandler.fromURL(this.client, this.searchHost, author = authorName)
         val searchResult = handler.execute()
-        val authorResult = searchResult.find { it.author != null && it.author?.asin != "search" } ?: return null
-        return getAuthorInfo(authorResult.author!!.asin)
+        val authorResult = searchResult.filter { it.author != null && it.author?.asin != "search" }
+        if (authorResult.isEmpty()) return null
+
+        val author = FuzzySearch.extractOne(authorName, authorResult) { it.author!!.name }
+        if (author.score < searchScore) return null
+
+        return getAuthorInfo(author.referent.author!!.asin)
+    }
+
+    suspend fun getBookByName(bookName: String): AudibleBook? {
+        val handler = SearchHandler.fromURL(this.client, this.searchHost, title = bookName)
+        val searchResult = handler.execute()
+        val bookResult = searchResult.filter { it.title != null && it.asin != "search" }
+        if (bookResult.isEmpty()) return null
+
+        val book = FuzzySearch.extractOne(bookName, bookResult) { it.title }
+        if (book.score < searchScore) return null
+
+        return getBookInfo(book.referent.asin)
     }
 
     suspend fun getBookInfo(bookASIN: String): AudibleBook {
@@ -58,8 +77,13 @@ open class AudibleClient(
     suspend fun getSeriesByName(seriesName: String): AudibleSeries? {
         val handler = SearchHandler.fromURL(this.client, this.searchHost, keywords = seriesName)
         val searchResult = handler.execute()
-        val seriesResult = searchResult.find { it.series != null && it.series?.asin != "search" } ?: return null
-        return getSeriesInfo(seriesResult.series!!.asin)
+        val seriesResult = searchResult.filter { it.series != null && it.series?.asin != "search" }
+        if (seriesResult.isEmpty()) return null
+
+        val series = FuzzySearch.extractOne(seriesName, seriesResult) { it.series!!.name }
+        if (series.score < searchScore) return null
+
+        return getSeriesInfo(series.referent.series!!.asin)
     }
 
     fun close() {
