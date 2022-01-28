83de59582705c743ded75962919dcc0b79fe8b0e
Niclas Haderer <niclas.haderer.dev@gmail.com>
2022-01-28 11:25:43 +0100

    Added scheduler for queuing async tasks

--- src/main/kotlin/io/github/huiibuh/Application.kt
+++ src/main/kotlin/io/github/huiibuh/Application.kt
@@ -10,18 +10,12 @@ import io.github.huiibuh.api.search.registerSearchRouting
 import io.github.huiibuh.api.stream.registerStreamingRouting
 import io.github.huiibuh.api.withBasePath
 import io.github.huiibuh.db.DatabaseFactory
-import io.github.huiibuh.db.tables.TAuthors
-import io.github.huiibuh.db.tables.TBooks
-import io.github.huiibuh.db.tables.TSeries
-import io.github.huiibuh.db.tables.TTracks
-import io.github.huiibuh.db.update.interceptor.withUpdateTime
 import io.github.huiibuh.di.configureKoin
 import io.github.huiibuh.file.scanner.CompleteScan
 import io.github.huiibuh.file.scanner.FileChangeService
 import io.github.huiibuh.logging.disableJAudioTaggerLogs
 import io.github.huiibuh.plugins.*
 import io.github.huiibuh.settings.getPort
-import io.github.huiibuh.ws.registerUpdateRoutes
 import io.ktor.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
@@ -35,7 +29,6 @@ fun main() {
         launch {
             DatabaseFactory.connect()
             DatabaseFactory.migrate()
-            withUpdateTime(TAuthors, TBooks, TSeries, TTracks)
             CompleteScan().start()
             FileChangeService().watch()
         }
@@ -55,7 +48,7 @@ fun Application.webServer() {
     configureSerialization()
     withBasePath("api",
         routeCallback = {
-            registerUpdateRoutes()
+//            registerUpdateRoutes()
         },
         openApiCallback = {
             apiRouting {
--- src/main/kotlin/io/github/huiibuh/utils/CoroutineScheduler.kt
+++ src/main/kotlin/io/github/huiibuh/utils/CoroutineScheduler.kt
@@ -1,20 +1,37 @@
 package io.github.huiibuh.utils
 
+import io.github.huiibuh.extensions.classLogger
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.async
+import kotlinx.coroutines.sync.Semaphore
 
-class CoroutineScheduler(val limit: Int) {
+class Scheduler(parallelism: Int) {
+    private val semaphore = Semaphore(parallelism)
+    private val log = classLogger()
 
-    private val scope = CoroutineScope(Dispatchers.IO)
-    private val taskList = mutableListOf<Any>()
-
-
-    fun schedule(callback: suspend CoroutineScope.() -> Any) {
-        scope.launch {
-            synchronized(this@CoroutineScheduler) {
-
+    suspend fun <T> queueAsync(callback: suspend () -> T): Deferred<T?> {
+        semaphore.acquire()
+        return CoroutineScope(Dispatchers.IO).async {
+            val deferred = async {
+                try {
+                    callback()
+                } catch (e: Exception) {
+                    this@Scheduler.log.error("Queued task caused an exception", e)
+                    null
+                }
+            }
+            return@async try {
+                deferred.await()
+            } finally {
+                semaphore.release()
             }
         }
+    }
+
+    @Suppress("DeferredResultUnused")
+    suspend fun queue(callback: suspend () -> Any) {
+        this.queueAsync(callback)
     }
 }
