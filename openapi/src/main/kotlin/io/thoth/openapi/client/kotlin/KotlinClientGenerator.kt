68017eef13c0c2aa2cf1ffc70edf0349846c1088
Niclas Haderer <niclas.haderer.dev@gmail.com>
2023-10-29 14:21:57 +0100

    fix: request runner is added now

--- openapi/src/main/kotlin/io/thoth/openapi/client/kotlin/KotlinClientGenerator.kt
+++ openapi/src/main/kotlin/io/thoth/openapi/client/kotlin/KotlinClientGenerator.kt
@@ -1,5 +1,6 @@
 package io.thoth.openapi.client.kotlin
 
+import io.ktor.client.request.*
 import io.thoth.openapi.client.common.ClientGenerator
 import io.thoth.openapi.client.common.ClientPart
 import io.thoth.openapi.ktor.OpenApiRoute
@@ -16,29 +17,18 @@ class KotlinClientGenerator(
     private val dist: Path,
 ) : ClientGenerator() {
 
-    private val reflections =
-        Reflections(
-            ConfigurationBuilder().forPackages("io.thoth.openapi.client.kotlin").addScanners(Scanners.Resources),
-        )
+    private val reflections = Reflections(
+        ConfigurationBuilder().forPackages("io.thoth.openapi.client.kotlin").addScanners(Scanners.Resources),
+    )
 
     private val requestRunner: String by lazy {
-        this::class
-            .java
-            .getResourceAsStream("/META-INF/resources/io/thoth/openapi/RequestRunner.kt")
-            ?.bufferedReader()
-            ?.readText()
-            ?: error("Cannot load RequestRunner.kt")
+        object {}.javaClass.getResourceAsStream("/RequestRunner.kt")?.bufferedReader()?.readText()
+            ?: error("Could not load RequestRunner.kt")
     }
 
     override fun generateClient(): List<ClientPart> {
-
-        // ALl resources of the project
-        val resources = reflections.getResources(Pattern.compile(".*\\.kt"))
-        println(resources)
-
         val parts = mutableListOf<ClientPart>()
-        parts += ClientPart(path = dist.resolve("client/kotlin/RequestRunner.kt"), content = requestRunner)
-
+        parts += ClientPart(path = dist.resolve("RequestRunner.kt"), content = requestRunner)
         return parts
     }
 }
@@ -48,9 +38,8 @@ fun generateKotlinClient(
     dist: Path,
 ) {
     KotlinClientGenerator(
-            routes = OpenApiRouteCollector.values(),
-            packageName = packageName,
-            dist = dist,
-        )
-        .safeClient()
+        routes = OpenApiRouteCollector.values(),
+        packageName = packageName,
+        dist = dist,
+    ).safeClient()
 }
