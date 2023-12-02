4c38a7c4197af514ca7ceb212a0cd376a7fac745
Niclas Haderer <niclas.haderer.dev@gmail.com>
2023-12-02 16:12:12 +0100

    tests: added some tests for the ts client generation

--- openapi/src/test/kotlin/io/thoth/openapi/TestApplication.kt
+++ openapi/src/test/kotlin/io/thoth/openapi/TestApplication.kt
@@ -1,19 +1,42 @@
 package io.thoth.openapi
 
 import io.ktor.resources.*
+import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.dataconversion.*
 import io.ktor.server.resources.Resources
 import io.ktor.server.routing.*
-import io.ktor.server.testing.*
+import io.thoth.openapi.ktor.Summary
 import io.thoth.openapi.ktor.get
+import io.thoth.openapi.ktor.post
 import io.thoth.openapi.ktor.plugins.OpenAPIRouting
 import io.thoth.openapi.serializion.kotlin.UUID_S
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.*
 
-@Resource("{path}") class Routes(val path: UUID_S, val name: String, val someParam: List<Int>)
+@Resource("{path}")
+class Routes(val path: UUID_S, val name: String, val someParam: List<Int>) {
+    @Summary(summary = "V1", method = "GET")
+    @Resource("V1")
+    class V1(val parent: Routes)
+
+    @Summary(summary = "V2", method = "GET")
+    @Resource("V2")
+    class V2(val parent: Routes)
+
+    @Summary(summary = "V3", method = "GET")
+    @Resource("V3")
+    class V3(val parent: Routes)
+
+    @Summary(summary = "V4", method = "GET")
+    @Resource("V4")
+    class V4(val parent: Routes)
+
+    @Summary(summary = "V5", method = "POST")
+    @Resource("V5")
+    class V5(val parent: Routes)
+}
 
 class ListRoute(val name: String, val someParam: List<UUID>)
 
@@ -27,7 +50,7 @@ class GenericRoute2<T, U>(val name: String, val someParam: T, val someParam2: U)
 
 class GenericRoute3<T>(val name: String, val someParam: Map<String, T>, val someParam2: GenericRoute2<T, String>)
 
-fun ApplicationTestBuilder.testRoutes() {
+fun Application.testRoutes() {
     install(Routing)
     install(DataConversion)
     install(Resources)
@@ -35,11 +58,10 @@ fun ApplicationTestBuilder.testRoutes() {
     install(OpenAPIRouting)
 
     routing {
-        get<Routes, ListRoute>("1") { TODO() }
-        get<Routes, MapRoute>("2") { TODO() }
-        get<Routes, SetRoute>("3") { TODO() }
-        get<Routes, GenericRoute<UUID>>("4") { TODO() }
-        get<Routes, GenericRoute2<LocalDate, UUID>>("5") { TODO() }
-        get<Routes, GenericRoute3<LocalDateTime>>("6") { TODO() }
+        get<Routes.V1, ListRoute> { TODO() }
+        get<Routes.V2, MapRoute> { TODO() }
+        get<Routes.V3, SetRoute> { TODO() }
+        get<Routes.V4, GenericRoute<UUID>> { TODO() }
+        post<Routes.V5, GenericRoute2<LocalDate, UUID>, GenericRoute3<LocalDateTime>> { _, _ -> TODO() }
     }
 }
--- openapi/src/test/kotlin/io/thoth/openapi/TsGenerationTest.kt
+++ openapi/src/test/kotlin/io/thoth/openapi/TsGenerationTest.kt
@@ -2,25 +2,61 @@ package io.thoth.openapi
 
 import io.ktor.server.testing.*
 import io.thoth.openapi.client.typescript.generateTsClient
-import io.thoth.openapi.ktor.OpenApiRouteCollector
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class TsGenerationTest {
 
     @Test
     fun testSomething() {
-        testApplication {
-            testRoutes()
-        }
-
         val generatedStuff = mutableMapOf<String, String>()
+        testApplication {
+            application {
+                testRoutes()
+                generateTsClient(
+                    dist = "",
+                    fileWriter = { file, content -> generatedStuff[file.name] = content },
+                )
+            }
+        }
+        val models = """
+        import type { Pair } from "./utility-types";
+        export type UUID = `$\{string}-$\{string}-$\{string}-$\{string}-$\{string}`
+        
+        export interface ListRoute  {
+          name: string;
+          someParam: Array<UUID>;
+        }
+        
+        export interface MapRoute  {
+          name: boolean;
+          someParam: Record<string, UUID>;
+        }
+        
+        export interface SetRoute  {
+          name: number;
+          someParam: Array<UUID>;
+        }
+        
+        export interface GenericRoute<T>  {
+          name: Pair<any, UUID>;
+          someParam: T;
+        }
+        
+        export interface GenericRoute2<T , U>  {
+          name: string;
+          someParam: T;
+          someParam2: U;
+        }
+        
+        export interface GenericRoute3<T>  {
+          name: string;
+          someParam: Record<string, T>;
+          someParam2: GenericRoute2<T, string>;
+        }
+        """.trimIndent().replace("\\{", "{")
 
-        generateTsClient(
-            dist = "",
-            fileWriter = { file, content -> generatedStuff[file.name] = content },
-        )
-
-        println(generatedStuff["models.ts"])
-
+        val generatedModels = generatedStuff["models.ts"]
+        assertEquals(models, generatedModels)
     }
 }
